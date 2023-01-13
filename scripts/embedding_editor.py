047e3bb4e9ca3ef11bdbbfc7bf1b554dda99026d
Dave Lage (rockerBOO) <rockerboo@gmail.com>
2023-01-12 22:11:07 -0500

    Add optimizer to save_embedding
    Optimizer was added in bddebe09edeb6a18f2c06986d5658a7be3a563ea.

--- scripts/embedding_editor.py
+++ scripts/embedding_editor.py
@@ -248,7 +248,9 @@ def save_embedding_weights(embedding_name, vector_num, *weights):
     checkpoint = sd_models.select_checkpoint()
     
     filename = os.path.join(shared.cmd_opts.embeddings_dir, f'{embedding_name}.pt')
-    save_embedding(embedding, checkpoint, embedding_name, filename, remove_cached_checksum=True)
+    optimizer = torch.optim.AdamW([embedding.vec])
+
+    save_embedding(embedding, optimizer, checkpoint, embedding_name, filename, remove_cached_checksum=True)
 
 def update_guidance_embeddings(text):
     try:
